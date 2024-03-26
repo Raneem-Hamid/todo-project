927b0bc6d58be9081442aa8f9a18d1f0d5bfa0cb
Raneem-Hamid <raneemhamid511@gmail.com>
2024-03-26 11:48:07 +0300

    solving lab06

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
 let Name = prompt("Please enter your name");
 let gender = prompt("pleas enter your gender");
 if (gender !== "male" && gender !== "female") {
-      gender = prompt(" please the gender only male or female");
+    gender = prompt(" please the gender only male or female");
 }
 let text;
 if (gender === "male") {
-      text = ("Mr.")
+    text = ("Mr.")
 }
 else if (gender === "female") {
-      text = ("Ms.")
+    text = ("Ms.")
 }
 let age = prompt("pleas enter your age");
 if (age <= 0) {
-      age = prompt("pleas enter your real age");
+    age = prompt("pleas enter your real age");
 }
 let skip = confirm("do you want to see the welcoming message?");
 if (skip) {
-      let welcoming = alert(" welcome " + text + Name);
+    let welcoming = alert(" welcome " + text + Name);
 
 }
+
+
+let Questions = [];
+Questions[0] = prompt(" Do you speak English?");
+Questions[1] = prompt("Do you speak Turkish?");
+Questions[2] = prompt("Do you speak German?");
+
+for (let i = 0; i < 3; i++) {
+    switch (Questions[i]) {
+        case "yes":
+        case "y":
+        case "YES":
+        case "Y":
+        case "Yes":
+            break;
+        case "no":
+        case "n":
+        case "NO":
+        case "N":
+        case "No":
+            break;
+        default:
+            Questions[i] = "invalid";
+    }
+}
+
+console.log(Questions);
 
