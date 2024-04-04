01d68fc5e77a705b742131ec3be5809f01765ada
Raneem-Hamid <raneemhamid511@gmail.com>
2024-04-04 13:40:15 +0300

    edit2

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-let Name = prompt("Please enter your name");
-let gender = prompt("pleas enter your gender");
-if (gender !== "male" && gender !== "female") {
-    gender = prompt(" please the gender only male or female");
-}
-let text;
-if (gender === "male") {
-    text = ("Mr.")
-}
-else if (gender === "female") {
-    text = ("Ms.")
-}
-let age = prompt("pleas enter your age");
-if (age <= 0) {
-    age = prompt("pleas enter your real age");
-}
-let skip = confirm("do you want to see the welcoming message?");
-if (skip) {
-    let welcoming = alert(" welcome " + text + Name);
+function personalQuestions(){
+    let personalAnswers = [];
+
+    let Name = prompt("Please enter your name");
+    personalAnswers.push(Name);
+
+    let gender = prompt("pleas enter your gender");
+    if (gender !== "male" && gender !== "female") {
+        gender = prompt(" please the gender only male or female");
+    }
+    personalAnswers.push(gender);
+
+    let text;
+    if (gender === "male") {
+        text = ("Mr.")
+    }
+    else if (gender === "female") {
+        text = ("Ms.")
+    }
+    let age = prompt("pleas enter your age");
+    if (age <= 0) {
+        age = prompt("pleas enter your real age");
+
+    }
+     personalAnswers.push(age);
+    let skip = confirm("do you want to see the welcoming message?");
+    if (skip) {
+        let welcoming = alert(" welcome " + text + Name);
+    
+    }
+
+    console.log(personalAnswers);
+
 
 }
+ 
+
+personalQuestions();
 
 
 function answers(){
